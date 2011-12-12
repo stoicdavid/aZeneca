c15561f980c9c7a79aaef5cb30cd5a82a998eb7b
David Rodriguez Elizalde <stoicdavid@gmail.com>
2011-12-12 00:35:41 -0600

    ajuste al regresar del pdf

--- VisualAid/HelperViewController.m
+++ VisualAid/HelperViewController.m
@@ -43,7 +43,7 @@
 
 
 - (void)previewControllerWillDismiss:(QLPreviewController *)controller {
-    [controller presentingViewController].view.frame = CGRectMake(0, 0, 1024, 768);
+    [controller presentingViewController].view.frame = CGRectMake(0, 20, 1024, 768);
 }
 
 - (void)dealloc
