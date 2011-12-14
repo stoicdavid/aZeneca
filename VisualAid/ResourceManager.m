48b40c4cbd7603f3dee11768df950973db9587b6
David Rodriguez Elizalde <stoicdavid@gmail.com>
2011-12-14 01:43:19 -0600

    fixed avi 2

--- VisualAid/ResourceManager.m
+++ VisualAid/ResourceManager.m
@@ -116,7 +116,7 @@
         
         aids = [[NSArray alloc] initWithObjects:@"AVNexiumMups", nil];
         aidsK = [[NSArray alloc] initWithObjects:@"Nexium Mups", nil];        
-        extras = [[NSArray alloc] initWithObjects:@"CampanaTV",@"RADipticoRIMA",@"RATarjetonAtlasparaiPad",@"RATarjetonNexiumMupsInstitucionales",@"RATripticoRIMA", nil];
+        extras = [[NSArray alloc] initWithObjects:@"CampanaTV",@"RADipticoRIMA",@"RATarjetonAtlasparaiPad",@"RATarjetonNexiumMups",@"RATripticoRIMA", nil];
         extrasK = [[NSArray alloc] initWithObjects:@"Campaña TV",@"Díptico RIMA",@"Tarjetón Atlas para iPad",@"Tarjetón Nexium Mups Institucionales",@"Tríptico RIMA", nil] ;        
         videos = [[NSArray alloc] initWithObjects:@"ComercialTVNexium", nil];
         videosK = [[NSArray alloc] initWithObjects:@"Comercial TV Nexium", nil] ;        
